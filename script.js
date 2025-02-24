4a95f8a7e4110aa5f9d0da17f1073adb63488e7e
kpisti18 <k.istvanzoltan@gmail.com>
2025-02-24 11:41:58 +0100

    index.html, script.js, style.css hozzáadva

---
+++ script.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const containerDiv = document.createElement('div');
+    containerDiv.classList.add('container');
+
+    const demoSpan = document.createElement('span');
+    demoSpan.id = 'demo';
+
+    const textarea = document.createElement('textarea');
+    textarea.name = 'area';
+    textarea.id = 'area';
+    textarea.placeholder = 'Írd be a jelszavad:';
+
+    containerDiv.append(demoSpan, textarea);
+
+    //console.log(containerDiv);
+    document.body.append(containerDiv);
+});
