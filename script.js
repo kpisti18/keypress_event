fe1b26bff9d3fd4ed3c37f2c79121dc979f23054
kpisti18 <k.istvanzoltan@gmail.com>
2025-02-27 09:10:19 +0100

    script.js modified to keyup event

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ window.addEventListener('DOMContentLoaded', () => {
     textarea.name = 'area';
     textarea.id = 'area';
     textarea.placeholder = 'Írd be a jelszavad:';
-    textarea.addEventListener('keypress', () => jelszo(textarea, demoSpan));
+    textarea.addEventListener('keyup', () => jelszo(textarea, demoSpan));
 
     containerDiv.append(demoSpan, textarea);
 
@@ -21,6 +21,8 @@ function jelszo(textarea, demoSpan) {
     let csillag = '';
 
     for (let i = 0; i < textarea.value.length; i++) {
-        
+        csillag += '*';
     }
+
+    demoSpan.textContent = csillag;
 }
